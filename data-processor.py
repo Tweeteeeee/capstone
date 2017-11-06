f151464affe15e6b50e34855a3ce997390b88571
Yun Zhou <zhyun1990@gmail.com>
2017-11-06 00:13:26 -0800

    Fix format

--- data-processor.py
+++ data-processor.py
@@ -15,17 +15,17 @@ data_table = 'tweet'
 contact_points = '127.0.0.1'
 
 def list_to_str(input_list):
-   	result = ''
-	is_first = True
-	for item in input_list:
-		if not is_first:
-			result += ','
-		result += item
-		is_first = False
-	return result
+    result = ''
+    is_first = True
+    for item in input_list:
+        if not is_first:
+            result += ','
+        result += item
+        is_first = False
+    return result
 
 if __name__ == '__main__':
-	# - setup command line arguments
+    # - setup command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('key_space', help='the keyspace to use in cassandra')
     parser.add_argument('data_table', help='the data table to use')
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     data_table = args.data_table
     contact_points = args.contact_points
 
-	# - initiate a cassandra session
+    # - initiate a cassandra session
     cassandra_cluster = Cluster(
         contact_points=contact_points.split(',')
     )
@@ -47,16 +47,16 @@ if __name__ == '__main__':
     parser = ttp.Parser()
     
     for item in query_result:
-    	tweet = item.tweet_text
-    	logger.info('original tweet text: %s\n' % tweet)
-    	if tweet is None:
-    		continue
-    	parsed_entities = parser.parse(tweet)
-    	hashtags = parsed_entities.tags
-    	tags = list_to_str(hashtags)
-    	logger.info('hashtags: %s', tags)
+        tweet = item.tweet_text
+        logger.info('original tweet text: %s\n' % tweet)
+        if tweet is None:
+            continue
+        parsed_entities = parser.parse(tweet)
+        hashtags = parsed_entities.tags
+        tags = list_to_str(hashtags)
+        logger.info('hashtags: %s', tags)
 
-    	user_mentions = parsed_entities.users
-    	mentions = list_to_str(user_mentions)
-    	logger.info('mentions: %s', mentions)
+        user_mentions = parsed_entities.users
+        mentions = list_to_str(user_mentions)
+        logger.info('mentions: %s', mentions)
 
